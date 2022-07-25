c000a2d6f594139d2c0397331a2f9a7915a1a8e9
Yeho <yehok117@gmail.com>
2022-07-25 10:24:31 +0900

    solved 18108

---
+++ acmicpc/18108/18108.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cin.sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a;
+    cin >> a;
+
+    cout << a - 543 << endl;
+}
