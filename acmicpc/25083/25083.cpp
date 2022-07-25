b419b4b6999bb8fd0dcb5844a5ed67933874a9c8
Yeho <yehok117@gmail.com>
2022-07-25 10:33:28 +0900

    solved 10926, 25083

---
+++ acmicpc/25083/25083.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cin.sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout << "         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |" << endl;
+}
