03eb924a94f2fe27b61523e5dd64957e0efcef39
Yeho Gwon <devstita@gmail.com>
2022-07-27 13:48:43 +0900

    solved 24416

---
+++ acmicpc/24416/24416.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#define endl "\n"
+
+using namespace std;
+
+size_t fibo(int n) {
+    size_t fibo[100] = {0, 1, 1, 0, }; // fibo[n] = nth fibonacci sequence
+    for (int i = 3; i <= n; i++)
+        fibo[i] = fibo[i - 2] + fibo[i - 1];
+    return fibo[n];
+}
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    cout << fibo(N) << " " << N - 2 << endl;
+
+    return 0;
+}
