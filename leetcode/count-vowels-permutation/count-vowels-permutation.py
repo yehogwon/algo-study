775bdac69b5c18c9f1af0b315ef4e59f29b15333
Yeho Gwon <devstita@gmail.com>
2022-08-20 22:20:44 +0900

    enhanced a solution

--- leetcode/count-vowels-permutation/count-vowels-permutation.py
+++ leetcode/count-vowels-permutation/count-vowels-permutation.py
@@ -1,19 +1,13 @@
 # solved
 class Solution:
     def countVowelPermutation(self, n: int) -> int:
-        dp = [[0] * 5] * (n + 1)
-        def recursion(num): 
-            # a = e + i + u
-            # e = a + i
-            # i = e + o
-            # o = i
-            # u = i + o
-            if num == 1: 
-                return [1] * 5
-            if num == 2: 
-                return [3, 2, 2, 1, 2]
-            if dp[num][0] == 0: 
-                _a, _e, _i, _o, _u = recursion(num - 1)
-                dp[num] = [_e + _i + _u, _a + _i, _e + _o, _i, _i + _o]
-            return dp[num]
-        return sum(recursion(n)) % (10 ** 9 + 7)
+        dp = {'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1}
+        for i in range(2, n + 1): 
+            dp = {
+                'a': dp['e'] + dp['i'] + dp['u'], 
+                'e': dp['a'] + dp['i'],
+                'i': dp['e'] + dp['o'], 
+                'o': dp['i'], 
+                'u': dp['i'] + dp['o']
+            }
+        return sum(dp.values()) % (10 ** 9 + 7)
