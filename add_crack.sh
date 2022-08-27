e297989f17290439218367e0367c205ff9dd2e9f
Yeho Gwon <devstita@gmail.com>
2022-08-27 23:20:31 +0900

    editted add_crack.sh a line

--- add_crack.sh
+++ add_crack.sh
@@ -7,10 +7,10 @@ if [ $# -eq 0 ]
     echo "Arguments are required"
 else
   cd "${BASEDIR}/cracking-the-coding-interview"
-  for arg in "${@:2}"
+  for arg in "${@:3}"
     do
-      mkdir "$arg"
-      cp "../template/tem.${1}" "$arg/$arg.${1}"
+      mkdir "CH${2}/$arg"
+      cp "../template/tem.${1}" "CH${2}/$arg/$arg.${1}"
       echo "$arg"
     done
   cd - >> /dev/null
