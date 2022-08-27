63ab22b6c70e1fb1986121d936b2be1eeeafd2bd
Yeho Gwon <devstita@gmail.com>
2022-08-27 11:43:45 +0900

    implemented stack, queue in python

---
+++ cracking-the-coding-interview/return-kth-to-last/return-kth-to-last.py
@@ -0,0 +1,29 @@
+from typing import Tuple, Union
+import unittest
+
+
+
+def solution(root: Node, k: int) -> Union[int, str]: 
+    val_stack = []
+    cursor = root
+    while cursor is not None: 
+        val_stack.append(cursor.val)
+        cursor = cursor.next
+    for _ in range(k - 1): 
+        val_stack.pop()
+    return val_stack.pop()
+
+
+class SolutionTest(unittest.TestCase):
+    def __init__(self, methodName: str, param: Tuple) -> None:
+        super().__init__(methodName)
+        self.input, self.output = param[0], param[1]
+    def test_runs(self): 
+        self.assertEqual(solution(*self.input), self.output)
+
+if __name__ == '__main__': 
+    cases = [((LinkedList.create([1, 2, 3, 4, 5]), 2), 4), ((LinkedList.create(['a', 'b', 'd', 'e', 'g', 'c']), 3), 'e')] # edit here
+    suite = unittest.TestSuite()
+    for i, o in cases: 
+        suite.addTest(SolutionTest('test_runs', (i, o)))
+    unittest.TextTestRunner(verbosity=2).run(suite)
--- library/python/linear.py
+++ library/python/linear.py
@@ -1,4 +1,5 @@
-from typing import List, Union
+from collections import deque
+from typing import Deque, List, Optional, TypeVar, Union
 
 
 class Node: 
@@ -6,11 +7,12 @@ class Node:
         self.val = val
         self.next = next
     
-    def __str__(self) -> str:
+    def __str__(self, reverse=False) -> str:
         text = ''
         cursor = self
+        sep = ' -> ' if reverse is False else ' <- '
         while cursor is not None: 
-            text += str(cursor.val) + ' -> '
+            text += str(cursor.val) + sep
             cursor = cursor.next
         if len(text) > 0: 
             text = text[:-4]
@@ -58,3 +60,65 @@ class LinkedList:
             cur.next = Node(val)
             cur = cur.next
         root.next = head.next
+
+class Stack(): 
+    K = TypeVar('K')
+    def __init__(self) -> None:
+        self.__list: List[self.K] = []
+    
+    def push(self, val: K) -> None:
+        self.__list.append(val)
+
+    def pop(self) -> K: 
+        return self.__list.pop()
+    
+    def peek(self) -> Optional[K]: 
+        if self.empty(): 
+            return None
+        return self.__list[-1]
+    
+    def __len__(self) -> int:
+        return len(self.__list)
+    
+    def empty(self) -> bool: 
+        return len(self) == 0
+    
+    def __str__(self) -> str:
+        return str(self.__list)
+
+class Queue(): 
+    K = TypeVar('K')
+    def __init__(self) -> None:
+        self.__root = Node()
+        self.__cursor = self.__root
+        self.__len = 0
+    
+    def add(self, val: K) -> None: 
+        self.__cursor = self.__cursor.next
+        self.__cursor = Node(val)
+        self.__len += 1
+    
+    def peek(self) -> Optional[K]: 
+        if self.empty(): 
+            return None
+        return self.__root.next.val
+    
+    def remove(self) -> Optional[K]: 
+        if self.empty(): 
+            return None
+        val = self.__root.next.val
+        self.__root.next = self.__root.next.next
+        self.__len -= 1
+        return val
+    
+    def poll(self) -> Optional[K]: 
+        return self.remove()
+    
+    def __len__(self) -> int:
+        return self.__len
+    
+    def empty(self) -> bool:
+        return len(self) == 0
+    
+    def __str__(self) -> str:
+        return self.__root.next.__str__(reverse=True)
