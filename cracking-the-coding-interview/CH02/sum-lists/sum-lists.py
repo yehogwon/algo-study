a32d96958faf7f70e67d429a504db387bba2d08d
Yeho Gwon <devstita@gmail.com>
2022-08-28 16:56:29 +0900

    corrected : book : Sum Lists

--- cracking-the-coding-interview/CH02/sum-lists/sum-lists.py
+++ cracking-the-coding-interview/CH02/sum-lists/sum-lists.py
@@ -7,6 +7,7 @@ import unittest
 
 from linear import LinkedList, Node
 
+# Solution for the original problem
 def solution(head1: Node, head2: Node) -> Node: 
     ll: LinkedList[int] = LinkedList()
     cursor1, cursor2 = head1, head2
@@ -15,22 +16,36 @@ def solution(head1: Node, head2: Node) -> Node:
         num = 0
         if not cursor1: 
             num = cursor2.val
+            cursor2 = cursor2.next
         elif not cursor2: 
             num = cursor1.val
+            cursor1 = cursor1.next
         else: 
             num = cursor1.val + cursor2.val
-        ll.insert(0, (num + _up) % 10)
+            cursor1 = cursor1.next
+            cursor2 = cursor2.next
+        ll.add((num + _up) % 10)
         _up = (num + _up) // 10
-        cursor1 = cursor1.next
-        cursor2 = cursor2.next
     return ll.head
 
+# Solution for the follow-up problem
+def _solution(head1: Node, head2: Node) -> Node: 
+    pass
 
-class TestSolution(unittest.TestCase):
+
+class SolutionTest(unittest.TestCase):
+    def __init__(self, methodName: str, param: Tuple) -> None:
+        super().__init__(methodName)
+        self.input, self.output = param[0], param[1]
     def test_runs(self): 
-        cases = [((LinkedList(7, 1, 6).head, LinkedList(5, 9, 2).head), LinkedList(9, 1, 2).head)]
-        for i, o in cases: 
-            self.assertEqual(solution(*i), o)
+        self.assertEqual(solution(*self.input).tolist(), self.output.tolist())
 
 if __name__ == '__main__': 
-    unittest.main()
+    cases = [
+        ((LinkedList(7, 1, 6).head, LinkedList(5, 9, 2).head), LinkedList(2, 1, 9).head), 
+        ((LinkedList(8, 1).head, LinkedList(5).head), LinkedList(3, 2).head)
+    ] # edit here
+    suite = unittest.TestSuite()
+    for i, o in cases: 
+        suite.addTest(SolutionTest('test_runs', (i, o)))
+    unittest.TextTestRunner(verbosity=2).run(suite)
